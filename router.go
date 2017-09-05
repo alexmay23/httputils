13553a22532d086d526a6a998646c16624881424
Alex Moiseenko <alexmoiseenko@me.com>
2017-09-05 13:33:17 +0300

    fix

--- router.go
+++ router.go
@@ -5,30 +5,30 @@ import (
 	"net/http"
 )
 
-type router struct {
+type Router struct {
 	router *httprouter.Router
 }
 
-func (self *router) Get(path string, handler http.Handler) {
+func (self *Router) Get(path string, handler http.Handler) {
 	self.router.GET(path, wrapHandler(handler))
 }
 
-func (self *router) Post(path string, handler http.Handler) {
+func (self *Router) Post(path string, handler http.Handler) {
 	self.router.POST(path, wrapHandler(handler))
 }
 
-func (self *router) Put(path string, handler http.Handler) {
+func (self *Router) Put(path string, handler http.Handler) {
 	self.router.PUT(path, wrapHandler(handler))
 }
 
-func (self *router) Delete(path string, handler http.Handler) {
+func (self *Router) Delete(path string, handler http.Handler) {
 	self.router.DELETE(path, wrapHandler(handler))
 }
 
-func (self *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (self *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	self.router.ServeHTTP(w, req)
 }
 
-func NewRouter() *router {
-	return &router{httprouter.New()}
+func NewRouter() *Router {
+	return &Router{httprouter.New()}
 }
